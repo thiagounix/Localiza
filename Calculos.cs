f40058549c153b6c34619ae543ede90e04e12875
Thiago Gomes de Barros <thiago.barros@meta.com.br>
2021-05-13 19:05:44 -0300

    Adicionar arquivos de projeto.

---
+++ Calculos.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Localiza
+{
+    public static class Calculos
+    {
+        public static bool IsNumeroPrimo(int numero)
+        {
+            bool bPrimo = true;
+            int fator = numero / 2;
+            int i = 0;
+            for (i = 2; i <= fator; i++)
+            {
+                if ((numero % i) == 0)
+                    bPrimo = false;
+            }
+            return bPrimo;
+        }
+
+        static public ArrayList GetFatores(int num)
+        {
+            ArrayList vetor = new();
+           
+            for (int x = 1; x <= num; x++)
+            {
+                if (num % x == 0)
+                {
+                    vetor.Add(x);
+                }
+            }
+            return vetor;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+
+namespace Localiza
+{
+    class Program
+    {
+       static void Main(string[] args)
+        {
+            Console.Write("Digite um número: ");
+            var num = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            ArrayList vetor = new();
+            
+            var numfator = Calculos.GetFatores(num);
+            
+            foreach (var item in numfator)
+            {
+                if (Calculos.IsNumeroPrimo((int)(item)) == true)
+                    vetor.Add(item);
+            }
+           
+            Console.WriteLine($"Número de Entrada: \t {num}");
+
+
+            Console.Write($"Números divisores: ");
+            foreach (int item in numfator)
+            {
+                Console.Write(item+" ");
+            }
+         
+            Console.WriteLine();
+            Console.Write($"Divisores Primos: ");
+            foreach (int item in vetor)
+            {
+                Console.Write(item+" ");
+            }
+            Console.ReadKey();
+        }
+
+
+
+    }
+}
